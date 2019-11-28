52a0798501562f0aaa45715d8db7e5d30dc15fcc
ag1lo <aleksandra.greszta@lo1.sandomierz.pl>
2019-11-28 12:33:40 +0100

    instrukcja warunkowa w C++

--- C++/rownanie.cpp
+++ C++/rownanie.cpp
@@ -26,9 +26,27 @@
 
 using namespace std;
 
-int main(int argc, char **argv)
-{
-	
+int main(int argc, char **argv) {   
+    cout << "Witaj w C++";
+    int a;
+    int b;
+    float x;
+    cout << "Podaj liczbę a: ";
+    cin >> a;
+    cout << "Podaj liczbę b: ";
+    cin >> b;
+    if (a==0) {
+        if (b==0) {
+            cout << "równanie tożsamościowe";
+        } else {
+            cout << "równanie sprzeczne";
+        }
+    } else {
+        x=-b/(float)a;
+        cout << x;
+    }
 	return 0;
 }
+
+
 
